29a8ac75731265f466e597a1f7b9600d98548d40
Zakaria Rezk <zakariarezk0@gmail.com>
2025-04-05 02:06:41 +0200

    hotfix:: solve nav bar design issue

--- src/components/common/NavBarComponent.vue
+++ src/components/common/NavBarComponent.vue
@@ -2,34 +2,36 @@
   <header class="h-[150px] flex items-center">
     <nav
       aria-label="Main navigation "
-      class="text-center w-full  flex items-center justify-between xl:px-40 lg:px-20 md:px-12 px-4"
+      class="text-center w-full flex items-center justify-between xl:px-40 lg:px-20 md:px-12 px-4"
     >
-      <BaseButtonComponent
-        @click="toggleDarkMode"
-        size="fit"
-        variant="outline"
-        aria-label="Toggle dark mode"
-      >
-        <svg v-if="!isDark" class="w-6 h-6" fill="none" viewBox="0 0 24 24">
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-          />
-        </svg>
-        <svg v-else class="w-6 h-6" fill="none" viewBox="0 0 24 24">
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-          />
-        </svg>
-      </BaseButtonComponent>
-      <LogoComponent variant="primary" class="z-50" />
+      <div>
+        <BaseButtonComponent
+          @click="toggleDarkMode"
+          size="fit"
+          variant="outline"
+          aria-label="Toggle dark mode"
+        >
+          <svg v-if="!isDark" class="w-6 h-6" fill="none" viewBox="0 0 24 24">
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
+          </svg>
+          <svg v-else class="w-6 h-6" fill="none" viewBox="0 0 24 24">
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          </svg>
+        </BaseButtonComponent>
+        <LogoComponent variant="primary" class="z-50" />
+      </div>
       <div class="hidden lg:flex items-center">
         <List
           direction="horizontal"
